6071391bf0e7a11a91aab037712179e4b17f256d
Dominic German <dominicgerman@gmail.com>
2023-08-28 22:20:44 -0500

    Added more anthems and stuff

--- routes/services/[id].tsx
+++ routes/services/[id].tsx
@@ -1,69 +1,88 @@
-import { Fragment, h } from "preact";
-import { tw } from "twind";
-import { Handlers, PageProps } from "$fresh/server.ts";
+import { Fragment, h } from 'preact'
+import { tw } from 'twind'
+import { Handlers, PageProps } from '$fresh/server.ts'
 
-import { Service } from "../../utils/types.ts";
-import db from "../../static/db.json" assert { type: "json" };
+import { Service } from '../../utils/types.ts'
+import db from '../../static/db.json' assert { type: 'json' }
 
 export const handler: Handlers<{ service: Service }> = {
   GET(_req, ctx) {
-    const service = db.data.filter((item) => String(item.id) === ctx.params.id);
+    const service = db.data.filter((item) => String(item.id) === ctx.params.id)
     if (!service) {
-      return new Response("Services search failed", { status: 404 });
+      return new Response('Services search failed', { status: 404 })
     }
-    return ctx.render({ service: service[0] });
+    return ctx.render({ service: service[0] })
   },
-};
+}
 
 export default function Details(
   props: PageProps<{
-    service: Service;
-  }>,
+    service: Service
+  }>
 ) {
-  const service = props.data.service;
+  const service = props.data.service
   return (
     <div class="p-4 mx-auto max-w-screen-md font-mono font-mono">
       <div class="my-12 text-lg">
         <h1 class={tw`my-2 text-5xl`}>{service.title}</h1>
-        {service.special_notes
-          ? (
-            <div
-              class={tw`px-6 py-1 my-4 border border-[#ff0000] rounded-md bg-[#fa00001a]`}
-            >
-              <strong>Reminder:</strong> {service.special_notes}
-            </div>
-          )
-          : null}
+        {service.special_notes ? (
+          <div
+            class={tw`px-6 py-1 my-4 border border-[#ff0000] rounded-md bg-[#fa00001a]`}
+          >
+            <strong>Reminder:</strong> {service.special_notes}
+          </div>
+        ) : null}
       </div>
       <div class="rounded-md bg-[#f9f9f9] border border-[#c9d48a] p-4">
         <h2 class="text-xl">Anthem: {service.anthem_title}</h2>
-        {service.anthem_pdf
-          ? (
+        {service.anthem_pdf ? (
+          <a
+            class={tw`rounded-full inline-block my-6 px-6 py-3 bg-[#c9d48a]`}
+            href={`https://npcovenant-cc.s3.us-east-2.amazonaws.com/${service.anthem_pdf}.pdf`}
+            target="_blank"
+          >
+            SHEET MUSIC
+          </a>
+        ) : null}
+        {service.listen_link ? (
+          <a
+            class={tw`rounded-full inline-block m-6 px-6 py-3 bg-[#c9d48a]`}
+            href={service.listen_link}
+            target="_blank"
+          >
+            LISTEN
+          </a>
+        ) : null}
+      </div>
+      {service.other_music ? (
+        <div class="rounded-md bg-[#f9f9f9] border border-[#c9d48a] p-4 mt-8">
+          <h2 class="text-xl">{service.other_music}</h2>
+          {service.other_pdf ? (
             <a
               class={tw`rounded-full inline-block my-6 px-6 py-3 bg-[#c9d48a]`}
-              href={`https://npcovenant-cc.s3.us-east-2.amazonaws.com/${service.anthem_pdf}.pdf`}
+              href={`https://npcovenant-cc.s3.us-east-2.amazonaws.com/${service.other_pdf}.pdf`}
               target="_blank"
             >
               SHEET MUSIC
             </a>
-          )
-          : null}
-        {service.listen_link
-          ? (
+          ) : null}
+          {service.other_link ? (
             <a
               class={tw`rounded-full inline-block m-6 px-6 py-3 bg-[#c9d48a]`}
-              href={service.listen_link}
+              href={service.other_link}
               target="_blank"
             >
               LISTEN
             </a>
-          )
-          : null}
-      </div>
+          ) : null}
+        </div>
+      ) : null}
       <span class="text-xl rounded-md my-8 py-1 px-4 inline-block bg-[#f9f9f9] border border-[#c9d48a]">
         Call time: {service.call_time}
       </span>
-      <a class="block my-12 text-2xl" href="/">← Back</a>
+      <a class="block my-12 text-2xl" href="/">
+        ← Back
+      </a>
     </div>
-  );
+  )
 }
--- utils/types.ts
+++ utils/types.ts
@@ -1,13 +1,14 @@
 export type Service = {
-  id: number;
-  sort: number | null;
-  service_date: string;
-  title: string;
-  anthem_title: string;
-  anthem_pdf: string | null;
-  other_music: string | null;
-  other_pdf: string | null;
-  call_time: string | null;
-  special_notes: string | null;
-  listen_link: string | null;
-};
+  id: number
+  sort: number | null
+  service_date: string
+  title: string
+  anthem_title: string
+  anthem_pdf: string | null
+  other_music: string | null
+  other_pdf: string | null
+  call_time: string | null
+  special_notes: string | null
+  listen_link: string | null
+  other_link: string | null
+}
