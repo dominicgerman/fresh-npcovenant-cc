d09bdff4301ef059b0ef52a615025a2a6ca670de
Dominic German <dominicgerman@gmail.com>
2023-09-23 22:36:02 -0500

    fixed dates

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,27 +1,29 @@
-import { Head } from "$fresh/runtime.ts";
-import { tw } from "twind";
-import { Handlers, PageProps } from "https://deno.land/x/fresh@1.2.0/server.ts";
-import { Service } from "../utils/types.ts";
-import ServiceCard from "../components/ServiceCard.tsx";
-import db from "../static/db.json" assert { type: "json" };
+import { Head } from '$fresh/runtime.ts'
+import { tw } from 'twind'
+import { Handlers, PageProps } from 'https://deno.land/x/fresh@1.2.0/server.ts'
+import { Service } from '../utils/types.ts'
+import ServiceCard from '../components/ServiceCard.tsx'
+import db from '../static/db.json' assert { type: 'json' }
 
 export const handler: Handlers<{ services: Service[] }> = {
   GET(req, ctx) {
-    const services = db;
+    const services = db
     if (!services) {
-      return new Response("Services search failed", { status: 404 });
+      return new Response('Services search failed', { status: 404 })
     }
-    return ctx.render({ services: services.data });
+    return ctx.render({ services: services.data })
   },
-};
+}
 
 export default function Home(
   props: PageProps<{
-    services: Service[];
-  }>,
+    services: Service[]
+  }>
 ) {
-  const { services } = props.data;
-  const today = new Date();
+  const { services } = props.data
+  const today = Date.parse(new Date())
+  const SIX_HOURS = 21600000
+
   return (
     <>
       <Head>
@@ -33,17 +35,15 @@ export default function Home(
           class="w-32 h-32"
           alt="the north park covenant church logo: a cross in a circle"
         />
-        <h1 class="my-6 text-4xl">
-          NPCC Chancel Choir (Fall 2023)
-        </h1>
+        <h1 class="my-6 text-4xl">NPCC Chancel Choir (Fall 2023)</h1>
 
         <div class="p-6 rounded-md bg-[#fa00001a]">
           <strong class="text-2xl inline-block mb-4">Announcements</strong>
           {services.map((service) => {
-            const dateObj = new Date(service.service_date);
+            const dateObj = new Date(service.service_date)
             if (dateObj >= today && service.special_notes) {
-              return <p>- {service.special_notes}</p>;
-            } else return null;
+              return <p>- {service.special_notes}</p>
+            } else return null
           })}
         </div>
 
@@ -51,13 +51,14 @@ export default function Home(
 
         <div class={tw`my-12`}>
           {services.map((service) => {
-            const dateObj = new Date(service.service_date);
+            const dateObj =
+              Date.parse(new Date(service.service_date)) + SIX_HOURS
             if (dateObj >= today) {
-              return <ServiceCard key={service.id} service={service} />;
+              return <ServiceCard key={service.id} service={service} />
             }
           })}
         </div>
       </div>
     </>
-  );
+  )
 }
